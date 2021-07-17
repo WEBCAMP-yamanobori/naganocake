2f3193edc17fc30416bb5bd236c500af9d8c88d3
yuto-takami <orca.y1322@gmail.com>
2021-07-17 09:45:55 +0000

    アイテムコントローラ、モデル作成

---
+++ db/migrate/20210717092011_create_items.rb
@@ -0,0 +1,14 @@
+class CreateItems < ActiveRecord::Migration[5.0]
+  def change
+    create_table :items do |t|
+      t.integer :genre_id
+      t.string :name
+      t.integer :non_taxed_price
+      t.text :introduction
+      t.string :image_id
+      t.boolean :is_active
+
+      t.timestamps
+    end
+  end
+end
