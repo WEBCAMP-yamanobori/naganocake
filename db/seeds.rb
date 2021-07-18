1e4f43ddee70c1702323b97f77c1af0e64be8366
Hiroki1112Kurata <sequoia1112@yahoo.co.jp>
2021-07-18 11:05:13 +0000

    [Add]管理者側会員一覧

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,17 @@
 
 Admin.create!(
    email: 'admin@admin.com',
-   password: 'adminadmin',
+   password: 'adminadmin'
+)
+
+Customer.create!(
+   last_name: '田中',
+   first_name: '太郎',
+   last_name_kana: 'タナカ',
+   first_name_kana: 'タロウ',
+   email: 'tanakadmmwc202106@tanaka.com',
+   postcode: '1066108',
+   address: '東京都港区六本木６丁目１０−１',
+   phone_number: '1234567890',
+   password: '3edc6yhn'
 )
