541f047a689c3318169cc9376fb005aa8230fe53
yuto-takami <orca.y1322@gmail.com>
2021-07-18 05:33:10 +0000

    修正

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,28 +1,28 @@
 class Admin::ItemsController < ApplicationController
   def index
   end
-  
+
   def new
     @item = Item.new
   end
-  
+
   def create
     @item = Item.new(item_params)
     @item.save
     redirect_to admin_items_path
   end
-  
+
   def show
   end
-  
+
   def edit
   end
-  
+
   def update
   end
-  
-  
-  
+
+
+
   private
   def item_params
     params.require(:item).permit(:image, :name, :introduction, :genre, :non_taxed_price, :is_active)
