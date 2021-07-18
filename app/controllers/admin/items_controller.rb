70a82c2a4cde547aade102aa60d5bf2b4875fa48
yuto-takami <orca.y1322@gmail.com>
2021-07-18 04:20:26 +0000

    gemファイル修正

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,12 +1,15 @@
 class Admin::ItemsController < ApplicationController
+  def index
+  end
+  
   def new
     @item = Item.new
   end
   
   def create
-  end
-  
-  def index
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_items_path
   end
   
   def show
