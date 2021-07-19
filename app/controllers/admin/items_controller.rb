6c34ecbcfe21077d1e6b578c7ce82f5c605f7113
yuto-takami <orca.y1322@gmail.com>
2021-07-19 01:56:31 +0000

    レイアウト以外完成

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -16,18 +16,23 @@ class Admin::ItemsController < ApplicationController
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to admin_items_path(@item.id)
   end
 
 
 
   private
   def item_params
-    params.require(:item).permit(:image, :name, :introduction, :genre, :non_taxed_price, :is_active)
+    params.require(:item).permit(:image, :name, :introduction, :genre_id, :non_taxed_price, :is_active)
   end
 end
