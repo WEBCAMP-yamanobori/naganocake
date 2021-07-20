013870cdef07f500776fe5c7e2b3e384e5ae5bb4
yuto-takami <orca.y1322@gmail.com>
2021-07-20 06:26:30 +0000

    会員側の商品一覧、詳細画面完成

--- config/routes.rb
+++ config/routes.rb
@@ -3,19 +3,21 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => "public/homes#top"
   get "home/about" => "public/homes#about"
+  
 
   scope module: :public do
     get "customers/my_page" =>"customers#show"
     resource :customers, only: [:edit, :update]
     get "customers/unsubscribe" => "customers#unsubscribe"
     get "customers/withdraw" => "customers#withdraw"
+    resources :items, only: [:index, :show]
   end
 
   devise_for :customers
 
   namespace :admin do
     root :to => "homes#top"
-    resources :genres, only: [:index, :edit, :create, :update] 
+    resources :genres, only: [:index, :edit, :create, :update]
     resources :customers, only: [:index, :show, :edit, :update]
     resources :items, only: [:new, :create, :index, :show, :edit, :update]
   end
