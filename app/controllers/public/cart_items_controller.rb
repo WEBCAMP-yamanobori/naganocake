94afcdba760636a1c14cf026172a03e1f8782b52
yuto-takami <orca.y1322@gmail.com>
2021-07-21 15:34:44 +0000

    カート内商品一覧完成

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,34 +1,46 @@
 class Public::CartItemsController < ApplicationController
 
   def index
-    @cart_items = current_customer.cart_items.all
-    # @total = @items.inject(0) { |sum, item| sum + item.sum_of_price }
+    @cart_items = current_customer.cart_items
+    @total_price = @cart_items.inject(0) { |sum, item| sum + item.sum_of_price }
   end
 
   def create
-    @cart_item = current_user.cart_items.build(cart_item_params)
-    @cart_items = current_user.cart_items.all
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_items = current_customer.cart_items
     @cart_items.each do |cart_item|
-    if cart_item.item_id == @cart_item.item_id
-      new_quantity = cart_item.quantity + @cart_item.quantity
-      cart_item.update_attribute(:quantity, new_quantity)
-      @cart_item.delete
+      if cart_item.item_id == @cart_item.item_id
+        new_quantity = cart_item.quantity + @cart_item.quantity
+        cart_item.update_attribute(:quantity, new_quantity)
+        @cart_item.delete
+      end
     end
     @cart_item.save
     redirect_to cart_items_path
   end
 
   def update
+    puts params
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.update(cart_item_params)
+    redirect_to cart_items_path
   end
 
   def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    redirect_to cart_items_path
   end
 
   def destroy_all
+    @cart_items = current_customer.cart_items
+    @cart_items.destroy_all
+    redirect_to cart_items_path
   end
 
   private
-  def cart_items_params
+
+  def cart_item_params
     params.require(:cart_item).permit(:customer_id, :item_id, :quantity)
   end
 
